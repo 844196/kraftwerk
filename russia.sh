649e7f2f9c440c21b169a2c644ea0650d2481ec1
Masaya Takeda <844196@gmail.com>
2014-12-02 00:27:14 +0900

    ロシア語のとこを追加

---
+++ russia.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#
+#   @(#) ロシア語のとこ
+#
+#   Author:
+#       844196 (@84____)
+#
+#   License
+#       MIT
+#
+
+
+clear
+source ./library.sh; _CursorInvisible
+R="$(tput smso)$(tput setaf 1)  $(tput sgr 0)$(tput rmso)"
+
+function RTBON() {
+_MarginHeight 16; _MarginWidth 85
+local RTBON="
+${MARGIN_W}                                                                           $R$R$R$R
+${MARGIN_W}                                                                              $R
+${MARGIN_W}    $R$R$R$R$R              $R$R$R$R$R$R   $R$R$R$R$R       $R$R$R$R     $R$R    $R$R
+${MARGIN_W}  $R$R$R$R$R$R              $R$R$R$R$R$R   $R$R$R$R$R$R   $R$R$R$R$R$R   $R$R    $R$R
+${MARGIN_W}  $R$R    $R$R                  $R$R       $R$R    $R$R   $R$R    $R$R   $R$R$R  $R$R
+${MARGIN_W}  $R$R    $R$R                  $R$R       $R$R    $R$R   $R$R    $R$R   $R$R$R  $R$R
+${MARGIN_W}  $R$R    $R$R                  $R$R       $R$R$R$R$R$R   $R$R    $R$R   $R$R$R  $R$R
+${MARGIN_W}  $R$R$R$R$R$R                  $R$R       $R$R$R$R$R     $R$R    $R$R   $R$R$R$R$R$R
+${MARGIN_W}    $R$R$R$R$R                  $R$R       $R$R    $R$R   $R$R    $R$R   $R$R$R$R$R$R
+${MARGIN_W}    $R$R  $R$R                  $R$R       $R$R    $R$R   $R$R    $R$R   $R$R  $R$R$R
+${MARGIN_W}  $R$R    $R$R                  $R$R       $R$R    $R$R   $R$R    $R$R   $R$R  $R$R$R
+${MARGIN_W}  $R$R    $R$R                  $R$R       $R$R    $R$R   $R$R    $R$R   $R$R    $R$R
+${MARGIN_W}$R$R      $R$R                  $R$R       $R$R$R$R$R$R   $R$R$R$R$R$R   $R$R    $R$R
+${MARGIN_W}$R$R      $R$R                  $R$R       $R$R$R$R$R       $R$R$R$R     $R$R    $R$R
+"
+echo "${RTBON}"
+}
+
+function CnYFA() {
+_MarginHeight 12; _MarginWidth 94
+local CnYFA="
+${MARGIN_W}  $R$R$R$R$R          $R$R$R$R$R$R$R$R    $R$R    $R$R    $R$R$R$R$R$R$R$R$R        $R$R$R
+${MARGIN_W}$R$R$R$R$R$R$R        $R$R$R$R$R$R$R$R    $R$R    $R$R    $R$R$R$R$R$R$R$R$R      $R$R$R$R$R
+${MARGIN_W}$R$R      $R$R        $R$R        $R$R    $R$R    $R$R    $R$R$R                $R$R      $R$R
+${MARGIN_W}$R$R      $R$R        $R$R        $R$R    $R$R    $R$R    $R$R$R                $R$R      $R$R
+${MARGIN_W}$R$R                  $R$R        $R$R      $R$R$R$R      $R$R$R                $R$R      $R$R
+${MARGIN_W}$R$R                  $R$R        $R$R      $R$R$R$R      $R$R$R                $R$R      $R$R
+${MARGIN_W}$R$R                  $R$R        $R$R        $R$R        $R$R$R                $R$R$R$R$R$R$R
+${MARGIN_W}$R$R                  $R$R        $R$R        $R$R        $R$R$R                $R$R$R$R$R$R$R
+${MARGIN_W}$R$R      $R$R        $R$R        $R$R        $R$R        $R$R$R                $R$R      $R$R
+${MARGIN_W}$R$R      $R$R        $R$R        $R$R        $R$R        $R$R$R                $R$R      $R$R
+${MARGIN_W}$R$R$R$R$R$R$R    $R$R$R$R        $R$R        $R$R        $R$R$R                $R$R      $R$R
+${MARGIN_W}  $R$R$R$R$R      $R$R$R$R        $R$R        $R$R        $R$R$R                $R$R      $R$R
+"
+echo "${CnYFA}"
+}
+
+function PAbOTHNK() {
+_MarginHeight 12; _MarginWidth 121
+local PAbOTHNK="
+${MARGIN_W}$R$R$R$R$R         $R$R$R       $R$R$R$R$R$R     $R$R$R$R     $R$R$R$R$R$R   $R$R    $R$R   $R$R    $R$R   $R$R      $R$R
+${MARGIN_W}$R$R$R$R$R$R     $R$R$R$R$R     $R$R$R$R$R$R   $R$R$R$R$R$R   $R$R$R$R$R$R   $R$R    $R$R   $R$R    $R$R   $R$R      $R$R
+${MARGIN_W}$R$R    $R$R   $R$R      $R$R   $R$R           $R$R    $R$R       $R$R       $R$R    $R$R   $R$R  $R$R$R   $R$R    $R$R
+${MARGIN_W}$R$R    $R$R   $R$R      $R$R   $R$R           $R$R    $R$R       $R$R       $R$R    $R$R   $R$R  $R$R$R   $R$R    $R$R
+${MARGIN_W}$R$R    $R$R   $R$R      $R$R   $R$R$R$R$R     $R$R    $R$R       $R$R       $R$R$R$R$R$R   $R$R  $R$R$R   $R$R  $R$R
+${MARGIN_W}$R$R$R$R$R$R   $R$R      $R$R   $R$R$R$R$R$R   $R$R    $R$R       $R$R       $R$R$R$R$R$R   $R$R$R$R$R$R   $R$R$R$R
+${MARGIN_W}$R$R$R$R       $R$R$R$R$R$R$R   $R$R    $R$R   $R$R    $R$R       $R$R       $R$R    $R$R   $R$R$R$R$R$R   $R$R$R$R
+${MARGIN_W}$R$R           $R$R$R$R$R$R$R   $R$R    $R$R   $R$R    $R$R       $R$R       $R$R    $R$R   $R$R$R  $R$R   $R$R  $R$R
+${MARGIN_W}$R$R           $R$R      $R$R   $R$R    $R$R   $R$R    $R$R       $R$R       $R$R    $R$R   $R$R$R  $R$R   $R$R    $R$R
+${MARGIN_W}$R$R           $R$R      $R$R   $R$R    $R$R   $R$R    $R$R       $R$R       $R$R    $R$R   $R$R    $R$R   $R$R    $R$R
+${MARGIN_W}$R$R           $R$R      $R$R   $R$R$R$R$R$R   $R$R$R$R$R$R       $R$R       $R$R    $R$R   $R$R    $R$R   $R$R      $R$R
+${MARGIN_W}$R$R           $R$R      $R$R   $R$R$R$R$R       $R$R$R$R         $R$R       $R$R    $R$R   $R$R    $R$R   $R$R      $R$R
+"
+echo "${PAbOTHNK}"
+}
+
+RTBON; _Sleep 0.9; clear
+CnYFA; _Sleep 0.9; clear
+RTBON; _Sleep 0.9; clear
+PAbOTHNK; _Sleep 0.9; clear
