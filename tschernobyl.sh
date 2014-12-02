d18731cfc51492fbe93cbf0398d7a1a2b1adea6d
sasairc <sasairc@ssiserver.moe.hm>
2014-12-03 06:24:17 +0900

    点滅を追加（ただし、描画が追いつかない）

--- tschernobyl.sh
+++ tschernobyl.sh
@@ -8,7 +8,6 @@
 #
 
 W="\033[48;5;255m  \033[m"
-BG="\033[38;5;255m|\033[m"
 COLS=$(tput cols)
 LINES=$(tput lines)
 if $(which sleepenh >/dev/null 2>&1); then
@@ -33,6 +32,19 @@ function _margin_height() {
 				echo " "
 			done
 		)
+}
+
+# 描画が追いつかない
+function init_flash_screen() {
+	HEIGHT=$(expr $COLS / 2)
+	FLASH=$(
+				for i in `seq 1 $LINES`; do
+					for j in `seq 1 $HEIGHT`; do
+							echo -n "$W"
+					done
+					echo -n '\n'
+				done
+			)
 }
 
 function tschernobyl() {
@@ -122,60 +134,65 @@ echo -en "${FUKUSHIMA}"
 
 function print_tschernobyl() {
 	tschernobyl
-	${SLEEP} 1.76 > /dev/null
+	${SLEEP} 1.78 > /dev/null
 	clear
 	for ((i = 0;i < 12; i++)); do
 		tschernobyl
 		${SLEEP} 0.076 > /dev/null
 		clear
+		echo -en "${FLASH}"
 		${SLEEP} 0.076 > /dev/null
 	done
 }
 
 function print_harrisburg() {
 	harrisburg
-	${SLEEP} 1.76 > /dev/null
+	${SLEEP} 1.78 > /dev/null
 	clear
 	for ((i = 0;i < 12; i++)); do
 		harrisburg
 		${SLEEP} 0.076 > /dev/null
 		clear
+		echo -en "${FLASH}"
 		${SLEEP} 0.076 > /dev/null
 	done
 }
 
 function print_sellafield() {
 	sellafield
-	${SLEEP} 1.76 > /dev/null
+	${SLEEP} 1.78 > /dev/null
 	clear
 	for ((i = 0;i < 12; i++)); do
 		sellafield
 		${SLEEP} 0.076 > /dev/null
 		clear
+		echo -en "${FLASH}"
 		${SLEEP} 0.076 > /dev/null
 	done
 }
 
 function print_hiroshima() {
 	hiroshima
-	${SLEEP} 1.76 > /dev/null
+	${SLEEP} 1.78 > /dev/null
 	clear
 	for ((i = 0;i < 12; i++)); do
 		hiroshima	
 		${SLEEP} 0.076 > /dev/null
 		clear
+		echo -en "${FLASH}"
 		${SLEEP} 0.076 > /dev/null
 	done
 }
 
 function print_fukushima() {
 	fukushima
-	${SLEEP} 1.76 > /dev/null
+	${SLEEP} 1.78 > /dev/null
 	clear
 	for ((i = 0;i < 12; i++)); do
 		fukushima
 		${SLEEP} 0.076 > /dev/null
 		clear
+		echo -en "${FLASH}"
 		${SLEEP} 0.076 > /dev/null
 	done
 }
@@ -204,13 +221,16 @@ clear
 trap 'clear; tput cnorm; exit 1' SIGINT
 tput civis
 _margin_height
+#init_flash_screen
 
 for ((step = 0; step < 4; step++)); do
 	print_strings $step
+	clear
 done
 
 for ((step = 0; step < 5; step++)); do
 	test $step -ne 3 && print_strings $step
+	clear
 done
 
 tput cnorm
