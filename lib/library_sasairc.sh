8c36cf0ed5e456d72a3b008502436a5e16636fb0
sasairc <sasairc@ssiserver.moe.hm>
2014-12-05 00:35:53 +0900

    lib/library_sasairc.shを追加

---
+++ lib/library_sasairc.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#    Author:
+#        sasairc (@sasairc2)
+#
+#    License
+#        MIT
+# 
+
+function _sleep() {
+	if $(which sleepenh > /dev/null 2>&1); then
+		SLEEP='sleepenh'
+	elif $(which usleep > /dev/null 2>&1); then
+		SLEEP='usleep'
+	else
+   		SLEEP='sleep'
+	fi
+	${SLEEP} ${1} > /dev/null
+}
+
+function _margin_width() {
+	local cols=$(tput cols)
+	local width=$(expr \( ${cols} - ${1} \) / 2)
+	MARGIN_W=$(
+			for i in `seq 1 ${width}`; do
+				echo -n " "
+			done
+		)
+}
+
+function _margin_height() {
+	local lines=$(tput lines)
+	local height=$(expr \( ${lines} - ${1} \) / 2)
+	MARGIN_H=$(
+			for i in `seq 1 ${height}`; do
+				echo " "
+			done
+		)
+}
+
+function _initscr() {
+	clear
+	trap 'tput cnorm; exit 1' SIGINT
+	trap 'tput cnorm; exit 0' EXIT
+	tput civis
+}
+
+function _endscr() {
+	clear
+	tput cnorm
+}
