3519abb97cf065aadff75c693b0e5355e2de0f1c
sasairc <sasairc@ssiserver.moe.hm>
2014-12-05 00:32:46 +0900

    らいぶ

---
+++ lib/library.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+#   @(#) 割と使う処理の汎用化テストライブラリ
+#
+#   Author:
+#       844196 (@84____)
+#
+#   License:
+#       MIT
+#
+
+
+function _CursorInvisible() {
+    trap "tput cnorm;" EXIT
+    trap "tput cnorm; exit 1" SIGINT
+    tput civis
+}
+
+function _Sleep() {
+    local t="${@}"
+    if $(which sleepenh >/dev/null 2>&1); then
+        local sleep='sleepenh'
+    else
+        local sleep='sleep'
+    fi
+    ${sleep} ${t} >/dev/null
+}
+
+function _MarginHeight() {
+    local height=$(tput lines)
+    local m=$(expr \( ${height} - ${1} \) / 2)
+    for i in $(seq 1 ${m}); do
+        echo " "
+    done
+}
+
+function _MarginWidth() {
+    local cols=$(tput cols)
+    local w=$(expr \( ${cols} - ${1} \) / 2)
+    MARGIN_W=$(for i in `seq 1 ${w}`; do printf " "; done)
+}
