60cfe539f913e83d72c0c510a2cd7d038110d605
sasairc <sasairc@ssiserver.moe.hm>
2014-12-07 00:05:41 +0900

    一応回るように

--- radio-activity/atom.sh
+++ radio-activity/atom.sh
@@ -7,10 +7,11 @@
 #        MIT
 #
  
-readonly E="\033[48;5;148m  \033[m"
+readonly E="\033[48;5;149m  \033[m"
+readonly EB="\033[48;5;6m  \033[m"
 readonly N="\033[48;5;1m  \033[m"
-readonly YP="\033[48;5;128m  \033[m"
-Y=""   # Dynamic variable
+readonly YP="\033[48;5;163m  \033[m"
+Y=""   # Dynamic variable (a, b, c, d, e, f, g, h)
 
 function _sleep() {
 	if $(which sleepenh > /dev/null 2>&1); then
@@ -60,53 +61,91 @@ _margin_height 33
 _margin_width 64
 local ATOM="
 ${MARGIN_H}
-${MARGIN_W}                $E$E$E                    $E$E$E                 #
-${MARGIN_W}              $E      $E                $E      $E               #
-${MARGIN_W}             $E        $E              $E        $E              #
-${MARGIN_W}             $E         $E            $E         $E              #
-${MARGIN_W}             $E           $E        $E           $E              #
-${MARGIN_W}             $E            $E      $E            $E              #
-${MARGIN_W}             $E             $E    $E             $E              #
-${MARGIN_W}              $E             $E  $E             $E               #
-${MARGIN_W}               $E             $E$E             $E                #
-${MARGIN_W}                $E             $E             $E                 #
-${MARGIN_W}                 $E          $E  $E          $E                  #
-${MARGIN_W}                  $E        $E    $E        $E                   #
-${MARGIN_W}             $E$E$E$E$E$E$E$E$E$E$E$E$E$E$E$E$E$E$E              #
-${MARGIN_W}       $E$E$E       $E    $E        $E    $E       $E$E$E        #
-${MARGIN_W}    $E$E             $E  $E   $Y$Y   $E  $E              $E$E    #
-${MARGIN_W}  $E                  $E$E  $Y$N$N$Y  $E$E                   $E  #
-${MARGIN_W}$E                     $E  $Y$N$N$N$Y  $E                      $E#
-${MARGIN_W}  $E                  $E$E  $Y$N$N$Y  $E$E                   $E  #
-${MARGIN_W}    $E$E             $E  $E   $Y$Y   $E  $E              $E$E    #
-${MARGIN_W}       $E$E$E       $E    $E        $E    $E       $E$E$E        #
-${MARGIN_W}             $E$E$E$E$E$E$E$E$E$E$E$E$E$E$E$E$E$E$E              #
-${MARGIN_W}                 $E         $E    $E         $E                  #
-${MARGIN_W}                $E           $E  $E           $E                 #
-${MARGIN_W}               $E              $E              $E                #
-${MARGIN_W}              $E              $E$E              $E               #
-${MARGIN_W}             $E              $E  $E               $E             #
-${MARGIN_W}            $E              $E    $E              $E             #
-${MARGIN_W}            $E             $E      $E             $E             #
-${MARGIN_W}            $E            $E        $E            $E             #
-${MARGIN_W}            $E          $E            $E          $E             #
-${MARGIN_W}            $E         $E              $E         $E             #
-${MARGIN_W}             $E      $E                  $E      $E              #
-${MARGIN_W}               $E$E$E                      $E$E$E                #
+${MARGIN_W}                $E$E$E                    $E$E$E                 
+${MARGIN_W}              $E      $E                $d      $E               
+${MARGIN_W}             $f        $E              $E        $e              
+${MARGIN_W}             $E         $E            $E         $E              
+${MARGIN_W}             $E           $E        $E           $E              
+${MARGIN_W}             $E            $E      $E            $E              
+${MARGIN_W}             $E             $g    $c             $E              
+${MARGIN_W}              $E             $E  $E             $E               
+${MARGIN_W}               $E             $E$E             $E                
+${MARGIN_W}                $e             $E             $E                 
+${MARGIN_W}                 $E          $E  $E          $f                  
+${MARGIN_W}                  $E        $E    $E        $E                   
+${MARGIN_W}             $h$E$E$E$E$E$E$E$E$a$E$E$E$E$E$E$E$E$b              
+${MARGIN_W}       $E$E$E       $E    $E        $E    $E       $E$E$E        
+${MARGIN_W}    $E$E             $E  $b   $Y$Y   $E  $E              $E$c    
+${MARGIN_W}  $E                  $E$E  $Y$N$N$Y  $E$E                   $E  
+${MARGIN_W}$E                     $E  $Y$N$N$N$Y  $E                      $E
+${MARGIN_W}  $E                  $E$E  $Y$N$N$Y  $E$E                   $E  
+${MARGIN_W}    $E$g             $E  $d   $Y$Y   $E  $E              $d$E    
+${MARGIN_W}       $E$E$E       $E    $E        $E    $E       $E$E$E        
+${MARGIN_W}             $E$E$E$E$E$E$E$E$E$f$E$E$E$E$E$E$E$e$E              
+${MARGIN_W}                 $E         $E    $E         $E                  
+${MARGIN_W}                $E           $E  $E           $E                 
+${MARGIN_W}               $a              $E              $h                
+${MARGIN_W}              $E              $E$E              $E               
+${MARGIN_W}             $E              $E  $c               $E             
+${MARGIN_W}            $E              $g    $E              $E             
+${MARGIN_W}            $E             $E      $E             $E             
+${MARGIN_W}            $E            $E        $E            $E             
+${MARGIN_W}            $E          $E            $E          $E             
+${MARGIN_W}            $h         $E              $b         $E             
+${MARGIN_W}             $E      $E                  $E      $a              
+${MARGIN_W}               $E$E$E                      $E$E$E                
 "
 echo -ne "${ATOM}"
 }
 
-
 function print_atom() {
-	for ((i = 0; i < 8; i++)); do
-		Y="  "
-		atom
-		_sleep 0.5
-		clear
+	for ((i = 0; i < 16; i++)); do
 		Y=$YP
+		a=$EB; b=$E; c=$E; d=$E; e=$E; f=$E; g=$E; h=$E
 		atom
-		_sleep 0.1
+		_sleep 0.2
+		clear
+
+		Y="  "
+		a=$E; b=$EB; c=$E; d=$E; e=$E; f=$E; g=$E; h=$E
+		atom
+		_sleep 0.2
+		clear
+
+		Y=$YP
+		a=$E; b=$E; c=$EB; d=$E; e=$E; f=$E; g=$E; h=$E
+		atom
+		_sleep 0.2
+		clear
+
+		Y="  "
+		a=$E; b=$E; c=$E; d=$EB; e=$E; f=$E; g=$E; h=$E
+		atom
+		_sleep 0.2
+		clear
+
+		Y=$YP
+		a=$E; b=$E; c=$E; d=$E; e=$EB; f=$E; g=$E; h=$E
+		atom
+		_sleep 0.2
+		clear
+
+		Y="  "
+		a=$E; b=$E; c=$E; d=$E; e=$E; f=$EB; g=$E; h=$E
+		atom
+		_sleep 0.2
+		clear
+	
+		Y=$YP
+		a=$E; b=$E; c=$E; d=$E; e=$E; f=$E; g=$EB; h=$E
+		atom
+		_sleep 0.2
+		clear
+
+		Y="  "
+		a=$E; b=$E; c=$E; d=$E; e=$E; f=$E; g=$E; h=$EB
+		atom
+		_sleep 0.2
 		clear
 	done
 }
